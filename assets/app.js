97bb9d6d8eb2b124a3ac13847b72bfebdc814be1
Madhurima <madhurimavispute61@gmail.com>
2024-01-25 19:01:18 +0530

    25 jan

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,10 @@
 document.getElementsByClassName("change1")[0].disabled = true;
 document.getElementsByClassName("change2")[0].disabled = true;
 document.getElementsByClassName("change3")[0].disabled = true;
-count=0  
+count1=0  
 document.getElementsByClassName("change")[0].onclick = function(){
     a = Math.random()
-    x = parseInt(a*6+1)
+    x = parseInt(a*3+1)
         document.getElementsByTagName("h1")[0].textContent = x
          document.getElementsByClassName("change")[0].disabled = true;
          document.getElementsByClassName("change1")[0].disabled = false;
@@ -13,15 +13,14 @@ document.getElementsByClassName("change")[0].onclick = function(){
 
     document.getElementsByClassName("change1")[0].onclick = function(){
         
-        count++
-    document.getElementsByClassName("count")[0].textContent = count
+        count1++
+    document.getElementsByClassName("count")[0].textContent = count1
     a = Math.random()
-    y =  parseInt(a*6+1)
+    y =  parseInt(a*3+1)
         document.getElementsByTagName("h1")[1].textContent = y
 
     if (x==y){
         document.getElementsByClassName("change1")[0].disabled = true;
         document.getElementsByClassName("change2")[0].disabled = false;
     }
-    
     }
--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,8 @@
 count=0  
-
 document.getElementsByClassName("change2")[0].onclick = function(){
   
     a = Math.random()
-    m = parseInt(a*6+1)
+    m = parseInt(a*3+1)
         document.getElementsByTagName("h1")[2].textContent = m
          document.getElementsByClassName("change2")[0].disabled = true;
          document.getElementsByClassName("change3")[0].disabled = false;
@@ -13,12 +12,35 @@ document.getElementsByClassName("change2")[0].onclick = function(){
         count++
     document.getElementsByClassName("count1")[0].textContent = count
     a = Math.random()
-    n =  parseInt(a*6+1)
+    n =  parseInt(a*3+1)
         document.getElementsByTagName("h1")[3].textContent = n
 
     if (m==n){
         document.getElementsByClassName("change3")[0].disabled = true;
-        
+
+        first = parseInt(document.getElementsByClassName("count")[0].textContent)
+        second = parseInt(document.getElementsByClassName("count1")[0].textContent)
+
+    if(first<second){
+        document.getElementsByClassName("res")[0].textContent = "Winner"
+        document.getElementsByClassName("res1")[0].textContent = "Loser"
+ }
+
+    else if(first==second){
+        document.getElementsByClassName("res")[0].textContent = "Tie"
+        document.getElementsByClassName("res1")[0].textContent = "Tie"
+ }
+ 
+    else{
+        document.getElementsByClassName("res")[0].textContent = "Loser"
+        document.getElementsByClassName("res1")[0].textContent = "Winner"
+ }
+
     }
-    }
+}
+ 
+
+
+
+
 
